6d4e43a873b4e35d97ea40b01c5e7f501fb6bda7
r3yz0n <reyzon.thapa2020@gmail.com>
2024-07-04 11:00:46 +0545

    Refactor ScrollPaneExample to improve code readability and maintainability

--- src/ScrollPaneExample.java
+++ src/ScrollPaneExample.java
@@ -7,7 +7,6 @@ public class ScrollPaneExample {
 		JFrame frame = new JFrame("JList with JScrollPane Demo");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(400, 300);
-		
 
 		DefaultListModel<String> listModel = new DefaultListModel<>();
 		for (int i = 0; i < 20; i++) {
@@ -18,12 +17,10 @@ public class ScrollPaneExample {
 		JList<String> list = new JList<>(listModel);
 //		list.setLayoutOrientation(JList.HORIZONTAL_WRAP); // For horizontal scrolling
 
-
 		// To make it scrolable we need to pass the list to the JscrolLpaen
 		JScrollPane scrollPane = new JScrollPane(list);
 
-		frame.add(scrollPane);  
-
+		frame.add(scrollPane);
 
 //		frame.add(list); // Doing this would not make it scrollable
 
